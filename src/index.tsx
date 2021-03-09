b6846acb5fcbe13af44037c0c181b4863ec4c96b
tuziel <tuziel@outlook.com>
2021-03-09 17:24:48 +0800

    update: 添加 eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -8,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
