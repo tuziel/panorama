d7f9ec580aa2dee707571fff29d32bc0b12b14b7
tuziel <tuziel@outlook.com>
2024-05-17 18:17:56 +0800

    chore: 提取取色器参数，顺便改进一下算法

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -1,6 +1,6 @@
 import { D180, D360 } from './consts';
 
-/**
+/*
  * three.js 坐标系定义
  * x 轴从左向右
  * y 轴从下向上
@@ -8,11 +8,36 @@ import { D180, D360 } from './consts';
  * phi 极角从 +y 开始
  * theta 方位角从 +z 开始向 +x 方向旋转
  */
+
 type Cartesian2 = [number, number];
 type Cartesian3 = [number, number, number];
 type Spherical = [number, number];
 type RGBA = [number, number, number, number];
 type ImageOrData = HTMLImageElement | ImageData;
+
+type ColorPicker = (x: number, y: number) => RGBA;
+
+type ColorPickerOprions = {
+  /**
+   * 滤镜
+   * - linear: 线性插值
+   * - nearest: 最近邻插值
+   */
+  filter?: 'linear' | 'nearest';
+
+  /**
+   * 坐标类型
+   * - uv: 纹理贴图坐标
+   * - xy: 像素坐标
+   */
+  coordinate?: 'uv' | 'xy';
+
+  /** 使用像素坐标时的目标宽度 */
+  dWidth?: number;
+
+  /** 使用像素坐标时的目标高度 */
+  dHeight?: number;
+};
 
 export enum Side {
   'RIGHT',
@@ -114,20 +139,21 @@ export function sphereImageToCubeImage(
 ) {
   const { width, height } = image;
   const outSize = size || Math.min(width / 4, height / 2);
+  const outSizeS1 = outSize - 1;
 
   const picker = createColorPicker(image);
 
   const imageData = new ImageData(outSize, outSize);
 
-  let phi, theta, x, y, color;
+  let phi, theta, u, v, color;
 
   for (let sx = 0; sx < outSize; sx++) {
     for (let sy = 0; sy < outSize; sy++) {
-      [phi, theta] = uvToSph(side, sx / outSize, sy / outSize);
-      x = ((((theta + D180) / D360) % 1) + 1) % 1;
-      y = (((phi / D180) % 1) + 1) % 1;
+      [phi, theta] = uvToSph(side, sx / outSizeS1, sy / outSizeS1);
+      u = ((((theta + D180) / D360) % 1) + 1) % 1;
+      v = (((phi / D180) % 1) + 1) % 1;
 
-      color = picker(x, y);
+      color = picker(u, v);
       setColor(imageData, sx, sy, color);
     }
   }
@@ -158,6 +184,8 @@ export function cubeImageToSphereImage(
   const baseSize = Math.min(width, height);
   const outWidth = (size || baseSize) * 4;
   const outHeight = (size || baseSize) * 2;
+  const outWidthS1 = outWidth - 1;
+  const outHeightS1 = outHeight - 1;
 
   const pickers = [
     createColorPicker(right),
@@ -170,15 +198,15 @@ export function cubeImageToSphereImage(
 
   const imageData = new ImageData(outWidth, outHeight);
 
-  let phi, theta, side, x, y, color;
+  let phi, theta, side, u, v, color;
 
   for (let sx = 0; sx < outWidth; sx++) {
     for (let sy = 0; sy < outHeight; sy++) {
-      phi = (sy / outHeight) * D180;
-      theta = (sx / outWidth) * D360 - D180;
-      [side, x, y] = SphToUv(phi, theta);
+      phi = (sy / outHeightS1) * D180;
+      theta = (sx / outWidthS1) * D360 - D180;
+      [side, u, v] = SphToUv(phi, theta);
 
-      color = pickers[side](x, y);
+      color = pickers[side](u, v);
       setColor(imageData, sx, sy, color);
     }
   }
@@ -192,7 +220,7 @@ export function cubeImageToSphereImage(
 export function canvasToImage(canvas: HTMLCanvasElement) {
   return new Promise<HTMLImageElement>((resolve, reject) => {
     canvas.toBlob((blob) => {
-      if (!blob) return reject(new Error('blob is undefined'));
+      if (!blob) throw new Error('blob is undefined');
 
       const reader = new FileReader();
       reader.addEventListener('error', reject);
@@ -245,25 +273,11 @@ export function loadImageData(src: string) {
   return loadImage(src).then(imageToData);
 }
 
-/**
- * 创建基于 uv 坐标的取色器
- * @param image 取色目标图片
- */
-export function createColorPicker(image: ImageOrData) {
-  const { width, height } = image;
-  const isImgElm = image instanceof HTMLImageElement;
-  const data = isImgElm ? imageToData(image).data : image.data;
+/** 线性插值 */
+export function createLinearPicker(image: ImageData) {
+  const { width, data } = image;
 
-  /**
-   * 取色器
-   * @param u 图片列位置
-   * @param v 图片行位置
-   */
-  function picker(u: number, v: number): RGBA {
-    const x = Math.min(Math.max(0, u * width - 0.5), width - 1);
-    const y = Math.min(Math.max(0, v * height - 0.5), height - 1);
-
-    // 线性插值
+  function picker(x: number, y: number): RGBA {
     const x1 = Math.floor(x);
     const x2 = Math.ceil(x);
     const y1 = Math.floor(y);
@@ -290,15 +304,120 @@ export function createColorPicker(image: ImageOrData) {
         data[irb + i] * prb;
     }
 
-    return color as RGBA;
+    return color;
   }
 
   return picker;
 }
 
+/** 最近邻插值 */
+export function createNearestPicker(image: ImageData) {
+  const { width, data } = image;
+
+  const picker: ColorPicker = (x: number, y: number) => {
+    const index = (Math.round(y) * width + Math.round(x)) * 4;
+
+    const color: RGBA = [
+      data[index],
+      data[index + 1],
+      data[index + 2],
+      data[index + 3],
+    ];
+
+    return color;
+  };
+
+  return picker;
+}
+
+/**
+ * 包装将纹理 uv 坐标映射到图片像素坐标的函数
+ * @param fn 操作函数
+ * @param width 纹理宽度
+ * @param height 纹理高度
+ */
+export function uvMapping<T>(
+  fn: (x: number, y: number) => T,
+  width: number,
+  height: number,
+) {
+  /**
+   * 取色器
+   * @param u 纹理贴图 u 坐标
+   * @param v 纹理贴图 v 坐标
+   */
+  return (u: number, v: number) => {
+    const x = clamp(u * width - 0.5, 0, width - 1);
+    const y = clamp(v * height - 0.5, 0, height - 1);
+    return fn(x, y);
+  };
+}
+
+/**
+ * 包装将图片像素坐标缩放的函数
+ * @param fn 操作函数
+ * @param width 原始宽度
+ * @param height 原始高度
+ * @param dWidth 目标宽度
+ * @param dHeight 目标高度
+ */
+export function xyMapping<T>(
+  fn: (x: number, y: number) => T,
+  width: number,
+  height: number,
+  dWidth = width,
+  dHeight = height,
+) {
+  /**
+   * 取色器
+   * @param x 图片像素 x 坐标
+   * @param y 图片像素 y 坐标
+   */
+  return (x: number, y: number) => {
+    const x2 = clamp(scaleIndex(x, dWidth / width), 0, width - 1);
+    const y2 = clamp(scaleIndex(y, dHeight / height), 0, height - 1);
+    return fn(x2, y2);
+  };
+}
+
+/**
+ * 创建取色器
+ * @param image 取色目标图片
+ * @param options 取色器参数
+ */
+export function createColorPicker(
+  image: ImageOrData,
+  options?: ColorPickerOprions,
+): ColorPicker {
+  const { filter, coordinate, dWidth, dHeight } = {
+    filter: 'linear',
+    coordinate: 'uv',
+    dWidth: 0,
+    dHeight: 0,
+    ...options,
+  };
+
+  const mapping = {
+    uv: uvMapping,
+    xy: xyMapping,
+  }[coordinate]!;
+
+  const creator = {
+    linear: createLinearPicker,
+    nearest: createNearestPicker,
+  }[filter]!;
+
+  const { width, height } = image;
+  const isImgElm = image instanceof HTMLImageElement;
+  const data = isImgElm ? imageToData(image) : image;
+  const xyPicker = creator(data);
+  const mappedPicker = mapping(xyPicker, width, height, dWidth, dHeight);
+
+  return mappedPicker;
+}
+
 export function setColor(image: ImageData, x: number, y: number, color: RGBA) {
-  const width = image.width;
-  const index = (y * width + x) * 4;
+  const index = (y * image.width + x) * 4;
 
   for (let i = 0; i < 4; i++) {
     image.data[index + i] = color[i];
@@ -307,9 +426,27 @@ export function setColor(image: ImageData, x: number, y: number, color: RGBA) {
 
 export function countBits(n: number) {
   let cnt = 0;
-  while (n) {
-    cnt++;
-    n >>= 1;
-  }
+  for (let m = n; m; m >>= 1) cnt++;
   return cnt;
+}
+
+/**
+ * 缩放像素坐标
+ * @param index 像素坐标
+ * @param scale 缩放比例
+ */
+export function scaleIndex(index: number, scale: number) {
+  return (index + 0.5) * scale - 0.5;
+}
+
+/**
+ * 保证数值在范围内
+ * @param number 数值
+ * @param lower 下界
+ * @param upper 上界
+ */
+export function clamp(number: number, lower: number, upper: number) {
+  if (number < lower) return lower;
+  if (number > upper) return upper;
+  return number;
 }
